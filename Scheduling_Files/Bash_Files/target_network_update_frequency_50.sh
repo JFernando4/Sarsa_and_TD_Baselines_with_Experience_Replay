9fa44d9ba940c20badf79b0d40333363a84492ff
jfernando <jfhernan@ualberta.ca>
2018-11-07 15:58:34 -0700

    scheduling files for target network update frequencies of 10 and 50

---
+++ Scheduling_Files/Bash_Files/target_network_update_frequency_50.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo Enter the number of agents you want to train:
+read number_of_agents
+echo Enter the number of the first agent:
+read first_agent_number
+last_agent_number=$(($first_agent_number + $number_of_agents - 1))
+
+export PYTHONPATH=.
+for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
+do
+    echo "Training Agent $i"
+    python3 ./sarsa_zero_mountain_car_control.py -episodes 500 \
+    -name Target_Network_Results/Target_Network_Update_Freq50/agent_$i \
+    -quiet -tnetwork_update_freq 50 -alpha 0.00025 -onpolicy -hidden_units 800 -max_steps 1000
+done
