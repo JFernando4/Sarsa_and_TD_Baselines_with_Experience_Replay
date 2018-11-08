aa2dd7f70a2698df9d5edd617f0dfc6c12ae408d
jfernando <jfhernan@ualberta.ca>
2018-11-08 06:14:45 -0700

    more scheduling files for replay start size parameter

--- Scheduling_Files/Bash_Files/Replay_Start_Size/replay_start_size_250.sh
+++ Scheduling_Files/Bash_Files/Replay_Start_Size/replay_start_size_250.sh
@@ -11,7 +11,7 @@ for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
 do
     echo "Training Agent $i"
     python3 ./sarsa_zero_mountain_car_control.py -episodes 500 \
-    -name Replay_Start_Size_Results/Replay_Start_Size_50/agent_$i \
+    -name Replay_Start_Size_Results/Replay_Start_Size_250/agent_$i \
     -quiet -tnetwork_update_freq 10 -alpha 0.00025 -onpolicy -hidden_units 800 -max_steps 1000 \
-    -replay_start 50
+    -replay_start 250
 done
