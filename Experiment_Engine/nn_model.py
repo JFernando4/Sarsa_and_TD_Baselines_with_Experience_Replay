f043624aa2d9ab72327459ca895a705d6573853f
jfernando <jfhernan@ualberta.ca>
2018-11-10 23:23:52 -0700

    initialization of the parameters for the control experiment is now done with N(0.1,0.1)

--- Experiment_Engine/nn_model.py
+++ Experiment_Engine/nn_model.py
@@ -9,7 +9,30 @@ def linear_transfer(x):
     return x
 
 
-def fully_connected(name, label, var_in, dim_in, dim_out, initializer, transfer, reuse=False, xavier_init=True):
+def fully_connected_av(name, label, var_in, dim_in, dim_out, initializer, transfer, reuse=False, xavier_init=True):
+    """Standard fully connected layer"""
+    with tf.variable_scope(name, reuse=reuse):
+        with tf.variable_scope(label, reuse=reuse):
+            if reuse:
+                W = tf.get_variable("W", [dim_in, dim_out])
+                b = tf.get_variable("b", [dim_out])
+            else: # new
+                if xavier_init:
+                    W = tf.get_variable("W", [dim_in, dim_out], initializer=initializer)
+                    b = tf.get_variable("b", [dim_out], initializer=initializer)
+                else:
+                    W = tf.get_variable("W", [dim_in, dim_out],
+                                        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
+                    b = tf.get_variable("b", [dim_out],
+                                        initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
+
+    z_hat = tf.matmul(var_in, W)
+    z_hat = tf.nn.bias_add(z_hat, b)
+    y_hat = transfer(z_hat)
+    return W, b, z_hat, y_hat
+
+
+def fully_connected_v(name, label, var_in, dim_in, dim_out, initializer, transfer, reuse=False, xavier_init=True):
     """Standard fully connected layer"""
     with tf.variable_scope(name, reuse=reuse):
         with tf.variable_scope(label, reuse=reuse):
@@ -81,7 +104,7 @@ class ActionValueFullyConnectedModel:
         current_y_hat = self.x_frames
         for j in range(self.full_layers):
             # layer n + m: fully connected
-            W, b, z_hat, y_hat = fully_connected(
+            W, b, z_hat, y_hat = fully_connected_av(
                 self.name, "full_" + str(j + 1), current_y_hat, dim_in[j], self.dim_out[j],
                 tf.random_normal_initializer(stddev=1.0 / np.sqrt(dim_in[j]), seed=SEED), self.gate_fun,
                 xavier_init=self.xavier_init)
@@ -92,7 +115,7 @@ class ActionValueFullyConnectedModel:
 
         """ Output layer """
         # output layer: fully connected
-        W, b, z_hat, self.y_hat = fully_connected(
+        W, b, z_hat, self.y_hat = fully_connected_av(
             self.name, "output_layer", current_y_hat, self.dim_out[-1], self.num_actions,
             tf.random_normal_initializer(stddev=1.0 / np.sqrt(self.dim_out[-1]), seed=SEED), linear_transfer,
             xavier_init=self.xavier_init)
@@ -107,7 +130,7 @@ class ActionValueFullyConnectedModel:
         # Temporal Difference Error
         self.td_error = tf.subtract(y, y_hat)
         # Loss
-        self.train_loss = tf.reduce_sum(tf.pow(self.td_error, 2))
+        self.train_loss = tf.reduce_mean(tf.pow(self.td_error, 2))
 
     def replace_model_weights(self, new_vars, tf_session=tf.Session()):
         if not isinstance(new_vars, list):
@@ -119,6 +142,7 @@ class ActionValueFullyConnectedModel:
 
     def get_variables_as_tensor(self):
         return self.train_vars[0]
+
 
 """
 Creates a model with k fully connected layers followed by one linear output layer for state values
@@ -165,7 +189,7 @@ class StateValueFullyConnectedModel:
         current_y_hat = self.x_frames
         for j in range(self.full_layers):
             # layer n + m: fully connected
-            W, b, z_hat, y_hat = fully_connected(
+            W, b, z_hat, y_hat = fully_connected_v(
                 self.name, "full_" + str(j + 1), current_y_hat, dim_in[j], self.dim_out[j],
                 tf.random_normal_initializer(stddev=1.0 / np.sqrt(dim_in[j]), seed=SEED), self.gate_fun,
                 xavier_init=self.xavier_init)
@@ -176,7 +200,7 @@ class StateValueFullyConnectedModel:
 
         """ Output layer """
         # output layer: fully connected
-        W, b, z_hat, self.y_hat = fully_connected(
+        W, b, z_hat, self.y_hat = fully_connected_v(
             self.name, "output_layer", current_y_hat, self.dim_out[-1], 1,
             tf.random_normal_initializer(stddev=1.0 / np.sqrt(self.dim_out[-1]), seed=SEED), linear_transfer,
             xavier_init=self.xavier_init)
@@ -185,13 +209,10 @@ class StateValueFullyConnectedModel:
         self.train_vars.extend([W, b])
         self.train_vars = [self.train_vars]
 
-        # Obtaining y_hat and Scaling by the Importance Sampling
-        y_hat = self.y_hat
-        y = self.y
         # Temporal Difference Error
-        self.td_error = tf.subtract(y, y_hat)
+        self.td_error = tf.subtract(self.y, self.y_hat)
         # Loss
-        self.train_loss = tf.reduce_sum(tf.pow(self.td_error, 2))
+        self.train_loss = tf.reduce_mean(tf.pow(self.td_error, 2))
 
     def replace_model_weights(self, new_vars, tf_session=tf.Session()):
         if not isinstance(new_vars, list):
