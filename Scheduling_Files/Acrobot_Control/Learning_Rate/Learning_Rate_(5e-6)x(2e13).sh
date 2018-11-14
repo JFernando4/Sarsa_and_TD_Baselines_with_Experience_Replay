d92af5bac8fdeaa1f3e4019a42399c5226d49b80
jfernando <jfhernan@ualberta.ca>
2018-11-14 14:52:28 -0700

    added scheduling files for more learning rates

---
+++ Scheduling_Files/Acrobot_Control/Learning_Rate/Learning_Rate_(5e-6)x(2e13).sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo Enter the number of agents you want to train:
+read number_of_agents
+echo Enter the number of the first agent:
+read first_agent_number
+last_agent_number=$(($first_agent_number + $number_of_agents - 1))
+
+export PYTHONPATH=.
+for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
+do
+    echo "Training Agent $i"
+    python3 ./sarsa_zero_acrobot_control.py -episodes 500 \
+    -name Learning_Rate_Results/Learning_Rate_"(5e-6)x(2e13)"/agent_$i \
+    -quiet -tnetwork_update_freq 100 -alpha "5e-6 * (2**13)" -onpolicy -hidden_units 2000 -max_steps 500 \
+    -replay_start 100
+done
